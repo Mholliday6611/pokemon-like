31287a2b92e8b0cb160082e40cac24e920f3906b
Axl <cruz.axl@gmail.com>
2018-07-25 13:49:07 -0400

    Added a change to movement

--- src/Player.js
+++ src/Player.js
@@ -12,10 +12,15 @@ class Player {
 		this.velocityY = 0
 		this.acceleration = 1;
 		this.direction = 5
+		this.xPosMove = true
+		this.xNegMove = true
+		this.yPosMove = true
+		this.yNegMove = true
 	}
 
 	render(){
 		this.game.rect(this.x, this.y, 50, 50);
+
 	}
 
 	update(npcs){
@@ -44,7 +49,7 @@ class Player {
 				this.direction = 8
 			}
 		}
-		if(this.game.keyIsDown(this.game.DOWN_ARROW)){
+		if(this.game.keyIsDown(this.game.DOWN_ARROW) && this.yNegMove == true){
 			if(this.velocityY < 4){
 				this.velocityY += .5
 				this.direction = 2
@@ -70,20 +75,34 @@ class Player {
 	}
 
 	collision(npcs){
-		if(this.x < 25 || this.x > this.game.width-25){
+		/*if(this.x < 25 || this.x > this.game.width-25){
 			this.velocityX = -this.velocityX 
 		}
 		if(this.y < 25 || this.y > this.game.height-25){
 			this.velocityY = -this.velocityY
 		}
-
+		*/
 		npcs.forEach(elem => {
-			if(this.game.dist(this.x,this.y,elem.x,elem.y) < 55){
-				this.velocityY = -this.velocityY
-				this.velocityX = -this.velocityX
-			
+			if(this.game.dist(this.x,this.y,elem.x,elem.y) < (elem.interactionBoxWidth/2 + 50/2))
+			{
+				if(this.y - elem.y < 20 && (this.x-elem.x < 5 || this.x-elem.x >-5 ))
+				{
+					this.yVelocity = 0
+					this.yNegMove = false 
+				}
 			}
+			else 
+			{
+				this.xPosMove = true
+				this.xNegMove = true
+				this.yPosMove = true
+				this.yNegMove = true
+			} 
+		
+
+
 			if(this.game.dist(this.x,this.y,elem.x,elem.y) < (elem.interactionBoxWidth/2 + 50/2)){
+
 				if(this.game.keyIsDown(32)){
 					alert(elem.dialogue)
 				}
